72b51960ca45fd888c35580dc443299ffd8612f9
remarkable-markup-1310 <wonsae.kim@gmail.com>
2019-04-02 13:15:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,4 +18,19 @@ WIN_COMBINATIONS = [
   def won?(board)
     win_combos = []
     for each win_combos in WIN_COMBINATIONS
+      win_index1 = win_combos[0]
+      win_index2 = win_combos[1]
+      win_index3 = win_combos[2]
+      
+      pos_1 = board[win_index1]
+      pos_2 = board[win_index2]
+      pos_3 = board[win_index3]
+      
+      if pos_1 == 'X' && pos_2 == 'X' && pos_3 == 'X'
+        return win_combos
+      elsif pos_1 == 'O' && pos_2 == 'O' && pos_3 == 'O'
+        return win_combos
+      else
+        false
+      end
   end
